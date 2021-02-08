d4912b8b9c2e2db34bb0ab1af6df3b32d0a3251c
Nahum <losond8@gmail.com>
2021-02-08 14:20:05 -0800

    Array Methods ( Add, Remove (im)mutable)

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = function(){
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+const destructivelyRemoveLastCat =() => {
+    cats.pop()
+}
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift()
+}
+
+const appendCat = function(){
+    const array = [...cats, "Broom"];
+   return array
+}
+
+const prependCat = function(){
+    const array = ["Arnold", ...cats];
+   return array
+}
+
+const removeLastCat = function(){
+   return  cats.slice(0,2)
+
+}
+
+const removeFirstCat = function(){
+    return  cats.slice(1,3)
+ 
+ }
